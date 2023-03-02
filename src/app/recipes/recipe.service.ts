6d4b40bfab9be010afe5d3053b838ac56f775f32
Jane-Bond <j.bondar1801@gmail.com>
2023-03-02 10:28:30 +0100

    adding a new project

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,77 @@
+import { Injectable } from "@angular/core";
+import { Subject } from "rxjs";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
+import { Recipe } from "./recipe.model";
+
+@Injectable()
+export class RecipeService {
+  recipesChanged = new Subject<Recipe[]>();
+
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     "Italian Pizza",
+  //     "Classic italian homemade pizza recipe",
+  //     "https://st3.depositphotos.com/2370557/16471/i/600/depositphotos_164719502-stock-photo-pepperoni-pizza-with-fresh-ingredients.jpg",
+  //     [
+  //       new Ingredient("dry yeast", 1),
+  //       new Ingredient("bread flour", 4),
+  //       new Ingredient("extra virgin olive oil", 2),
+  //       new Ingredient("tomato sauce", 1),
+  //       new Ingredient("mozzarella cheese", 1),
+  //       new Ingredient("parmesan cheese", 1),
+  //       new Ingredient("mushrooms", 4),
+  //       new Ingredient("olives", 5),
+  //     ]
+  //   ),
+  //   new Recipe(
+  //     "The Classic Burger",
+  //     "Best burger recipe",
+  //     "https://st3.depositphotos.com/1000504/18241/i/600/depositphotos_182414346-stock-photo-fresh-tasty-burger.jpg",
+  //     [
+  //       new Ingredient("beef", 1),
+  //       new Ingredient("onion", 1),
+  //       new Ingredient("hamburger buns", 4),
+  //       new Ingredient("ketchup", 1),
+  //       new Ingredient("mayonnaise", 1),
+  //       new Ingredient("lettuce leaves,", 2),
+  //       new Ingredient("firm-ripe tomato,", 1),
+  //     ]
+  //   ),
+  // ];
+  private recipes: Recipe[] = [];
+
+  constructor(private slService: ShoppingListService) {}
+
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+
+  getRecipe(index: number) {
+    return this.recipes[index];
+  }
+
+  addIngredientsToShoppingLIst(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
+  }
+
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  updateRecipe(index: number, newRecipe: Recipe) {
+    this.recipes[index] = newRecipe;
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  deleteRecipe(index: number) {
+    this.recipes.splice(index, 1);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+}
---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+
+import { RecipeService } from "../recipes/recipe.service";
+import { Recipe } from "../recipes/recipe.model";
+import { map } from "rxjs/operators";
+
+@Injectable({ providedIn: "root" })
+export class DataStorageService {
+  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+
+  storeRecipes() {
+    const recipes = this.recipeService.getRecipes();
+    this.http
+      .put(
+        "https://angular-cooking-app-2a43c-default-rtdb.europe-west1.firebasedatabase.app/recipes.json",
+        recipes
+      )
+      .subscribe((response) => {
+        console.log(response);
+      });
+  }
+
+  fetchRecipes() {
+    this.http
+      .get<Recipe[]>(
+        "https://angular-cooking-app-2a43c-default-rtdb.europe-west1.firebasedatabase.app/recipes.json"
+      )
+      .pipe(
+        map((recipes) => {
+          return recipes.map((recipe) => {
+            return {
+              ...recipe,
+              ingredients: recipe.ingredients ? recipe.ingredients : [],
+            };
+          });
+        })
+      )
+      .subscribe((recipes) => {
+        this.recipeService.setRecipes(recipes);
+      });
+  }
+}
