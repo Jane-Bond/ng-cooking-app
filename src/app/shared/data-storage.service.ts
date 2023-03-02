5a8906d3c4b0b820c7626c50dd5471928185662d
Jane-Bond <j.bondar1801@gmail.com>
2023-03-02 12:51:06 +0100

    resolving data before loading feature

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from "@angular/common/http";
 
 import { RecipeService } from "../recipes/recipe.service";
 import { Recipe } from "../recipes/recipe.model";
-import { map } from "rxjs/operators";
+import { map, tap } from "rxjs/operators";
 
 @Injectable({ providedIn: "root" })
 export class DataStorageService {
@@ -22,7 +22,7 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-    this.http
+    return this.http
       .get<Recipe[]>(
         "https://angular-cooking-app-2a43c-default-rtdb.europe-west1.firebasedatabase.app/recipes.json"
       )
@@ -34,10 +34,10 @@ export class DataStorageService {
               ingredients: recipe.ingredients ? recipe.ingredients : [],
             };
           });
+        }),
+        tap((recipes) => {
+          this.recipeService.setRecipes(recipes);
         })
-      )
-      .subscribe((recipes) => {
-        this.recipeService.setRecipes(recipes);
-      });
+      );
   }
 }
