6a835c53aa065aa69d3681eff0caed1718540799
Jane-Bond <j.bondar1801@gmail.com>
2023-03-07 10:23:38 +0100

    loading spinner added

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 
 interface AuthResponseData {
+  kind: string;
   idToken: string;
   email: string;
   refreshToken: string;
