886bb745f64b08af3524c5be894382cefc53be2b
Jane-Bond <j.bondar1801@gmail.com>
2023-03-07 11:19:13 +0100

    login requests and login error handling

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,43 +1,63 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { catchError } from "rxjs/operators";
 import { throwError } from "rxjs";
 
-interface AuthResponseData {
+export interface AuthResponseData {
   kind: string;
   idToken: string;
   email: string;
   refreshToken: string;
   expiresIn: string;
   localId: string;
+  registered?: boolean;
 }
 
 @Injectable({ providedIn: "root" })
 export class AuthService {
   constructor(private http: HttpClient) {}
 
-  signup(email: string, passowrd: string) {
+  signup(email: string, password: string) {
     return this.http
       .post<AuthResponseData>(
         "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBySJaOQL3zotQIkxS7SV-b9ASWyB_s2y8",
         {
           email: email,
-          password: passowrd,
+          password: password,
           returnSecureToken: true,
         }
       )
-      .pipe(
-        catchError((errorRes) => {
-          let errorMessage = "An unknown error occured!";
-          if (!errorRes.error || !errorRes.error.error) {
-            return throwError(errorMessage);
-          }
-          switch (errorRes.error.error.message) {
-            case "EMAIL_EXISTS":
-              errorMessage = "This email already exists";
-          }
-          return throwError(errorMessage);
-        })
-      );
+      .pipe(catchError(this.handleError));
+  }
+
+  login(email: string, password: string) {
+    return this.http
+      .post<AuthResponseData>(
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBySJaOQL3zotQIkxS7SV-b9ASWyB_s2y8",
+        {
+          email: email,
+          password: password,
+          returnSecureToken: true,
+        }
+      )
+      .pipe(catchError(this.handleError));
+  }
+
+  private handleError(errorRes: HttpErrorResponse) {
+    let errorMessage = "An unknown error occured!";
+    if (!errorRes.error || !errorRes.error.error) {
+      return throwError(errorMessage);
+    }
+    switch (errorRes.error.error.message) {
+      case "EMAIL_EXISTS":
+        errorMessage = "This email already exists";
+        break;
+      case "EMAIL_NOT_FOUND":
+        errorMessage = "This email is not found";
+        break;
+      case "INVALID_PASSWORD":
+        errorMessage = "Wrong password";
+    }
+    return throwError(errorMessage);
   }
 }
